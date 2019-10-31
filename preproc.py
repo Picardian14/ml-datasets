748d2d675b3d5fb430d580fdba1d4d1546abc466
picardian14 <mindlinster@gmail.com>
2019-10-31 10:21:09 -0300

    reduzco a la mitad

--- preproc.py
+++ preproc.py
@@ -5,29 +5,29 @@ sourceFolder = './train/pneumonia/'
 for filename in os.listdir(sourceFolder):
     im = Image.open(sourceFolder+filename)
     imFlip = im.transpose(Image.FLIP_LEFT_RIGHT)
-    crop = im.crop((100,100,500,500))
-    im.thumbnail((400,400))
+    crop = im.crop((100,100,300,300))
+    im.thumbnail((200,200))
     im.save(sourceFolder + filename)
 sourceFolder = './train/normal/'    
 for filename in os.listdir(sourceFolder):
     im = Image.open(sourceFolder+filename)
     imFlip = im.transpose(Image.FLIP_LEFT_RIGHT)
-    crop = im.crop((100,100,500,500))
-    im.thumbnail((400,400))
+    crop = im.crop((100,100,300,300))
+    im.thumbnail((200,200))
     im.save(sourceFolder + filename)
 sourceFolder = './val/pneumonia/'        
 for filename in os.listdir(sourceFolder):
     im = Image.open(sourceFolder+filename)
     imFlip = im.transpose(Image.FLIP_LEFT_RIGHT)
-    crop = im.crop((100,100,500,500))
-    im.thumbnail((400,400))
+    crop = im.crop((100,100,300,300))
+    im.thumbnail((200,200))
     im.save(sourceFolder + filename)
 sourceFolder = './val/normal/'        
 for filename in os.listdir(sourceFolder):
     im = Image.open(sourceFolder+filename)
     imFlip = im.transpose(Image.FLIP_LEFT_RIGHT)
-    crop = im.crop((100,100,500,500))
-    im.thumbnail((400,400))
+    crop = im.crop((100,100,300,300))
+    im.thumbnail((200,200))
     im.save(sourceFolder + filename)
 
 
